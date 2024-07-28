ecb7938be2bcd327ebacbe5e949447cac1061770
Clostro <81104065+ClostroOffi@users.noreply.github.com>
2024-07-28 12:52:57 +0200

    Update build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -4,7 +4,7 @@ runtimes=("win-x64" "win10-x64" "linux-x64" "linux-arm64" "osx.12-x64" "osx.12-a
 buildpath="src/bin/Release/net6.0"
 
 # Retrieving project version
-version=$(cat src/GICutscenes.csproj | grep -oP "<Version>\K([0-9]\.[0-9]\.[0-9])")
+version=$(cat src/ZZZCutscenes.csproj | grep -oP "<Version>\K([0-9]\.[0-9]\.[0-9])")
 
 for r in "${runtimes[@]}";
 do
@@ -12,11 +12,11 @@ do
 	echo "Building self contained $r"
 	rm -r "$buildpath/$r/publish"
 	dotnet publish -c Release -r $r --self-contained
-	zip -jr "GICutscenes-$version-$r-standalone.zip" "$buildpath/$r/publish"
+	zip -jr "ZZZCutscenes-$version-$r-standalone.zip" "$buildpath/$r/publish"
 
 	# Framework dependant
 	echo "Building framework dependant $r"
 	rm -r "$buildpath/$r/publish"
 	dotnet publish -c Release -r $r --self-contained false -p:PublishTrimmed=false
-	zip -jr "GICutscenes-$version-$r.zip" "$buildpath/$r/publish"
+	zip -jr "ZZZCutscenes-$version-$r.zip" "$buildpath/$r/publish"
 done
